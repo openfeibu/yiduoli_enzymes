d1607991192f7e1e94cf50902df27c61b0f97f1c
wzj <wzj@ea4cbc49-3bbf-484a-b0ce-f5dcab192e56>
2021-08-30 13:21:59 +0000

    git-svn-id: https://39.108.144.9:8443/svn/yiduoli_enzymes@41 ea4cbc49-3bbf-484a-b0ce-f5dcab192e56

---
+++ config/model/banner_vid.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+/*
+ * Modules .
+ */
+    'modules'  => ['banner_vid'],
+
+
+/*
+ * Views for the page  .
+ */
+    'views'    => ['default' => 'Default', 'left' => 'Left menu', 'right' => 'Right menu'],
+
+// Modale variables for page module.
+    'banner_vid'     => [
+        'model'        => 'App\Models\BannerVid',
+        'table'        => 'banner_vids',
+        'primaryKey'   => 'id',
+        'hidden'       => [],
+        'visible'      => [],
+        'guarded'      => ['*'],
+        //'slugs'        => ['slug' => 'name'],
+        'fillable'     => ['vid','name','order'],
+        'translate'    => [],
+        'upload_folder' => '/banner_vid',
+        'encrypt'      => ['id'],
+        'revision'     => ['title'],
+        'perPage'      => '20',
+        'search'        => [
+            'name' => 'like',
+            'vid'  => 'like',
+        ],
+    ],
+
+];
