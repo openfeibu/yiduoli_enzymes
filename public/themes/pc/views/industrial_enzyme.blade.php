6288a936a69d29e947b9386c5736a1fffa83a117
wzj <wzj@ea4cbc49-3bbf-484a-b0ce-f5dcab192e56>
2021-08-30 03:58:43 +0000

    git-svn-id: https://39.108.144.9:8443/svn/yiduoli_enzymes@34 ea4cbc49-3bbf-484a-b0ce-f5dcab192e56

--- public/themes/pc/views/industrial_enzyme.blade.php
+++ public/themes/pc/views/industrial_enzyme.blade.php
@@ -4,10 +4,9 @@
     <div class="container w1400">
         {!! Theme::widget('WebBreadcrumb')->render() !!}
     </div>
-    <div class="w1400 page-title clearfix wow fadeInLeft animated" data-wow-duration=".6s" data-wow-delay=".4s">
-        {!! Theme::widget('NavTab')->render() !!}
-    </div>
+
     <div class="container w1400">
+        {!! Theme::widget('NavTab')->render() !!}
         <div class="text-detail clearfix  wow fadeInUp animated animated" data-wow-duration=".6s" data-wow-delay=".5s">
             {!! $page['content'] !!}
         </div>
